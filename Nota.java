55144d22e9cb48fff52b8eebe8b06342054e4d8f
Caue-155 <143454637+Caue-155@users.noreply.github.com>
2024-03-12 11:08:42 -0300

    Add files via upload

---
+++ Nota.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        float n1 = 10;
+        float n2 = 5;
+        float n3 = 8;
+        float resultado = (n1 + n2 + n3) / 3; 
+        
+        System.out.println("O resultado é: " +resultado);
+    }
+}
